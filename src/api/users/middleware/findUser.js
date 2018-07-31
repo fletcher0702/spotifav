858a933e7ee61791f40055a60dbc45f289ecc657
ahzor <anatole.zha@gmail.com>
2018-07-31 14:59:38 +0200

    Debug encryption

--- src/api/users/middleware/findUser.js
+++ src/api/users/middleware/findUser.js
@@ -5,7 +5,7 @@ import cfg from './config';
 export default function (req, res, next) {
   usersServices
     .findUser(req.body.email, req.body.password)
-    .then((response) => { return { email: response.email, password: response.password }; })
+    .then((response) => { return { email: response.email, password: req.body.password }; })
     .then(payload => res.send({ token: jwt.encode(payload, cfg.jwtSecret) }))
     .catch(err => next(err));
 }
